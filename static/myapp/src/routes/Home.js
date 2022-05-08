e2ea2f949b11466f07cc8da7a173555af09556f8
kvv(Kevin Junho Kim) <59503331+kevinkim910408@users.noreply.github.com>
2022-05-08 19:14:46 -0400

    Home Component - make Empty except loading status
    Home Component - make Empty except loading status

--- static/myapp/src/routes/Home.js
+++ static/myapp/src/routes/Home.js
@@ -1,10 +1,25 @@
+// Components
 import Header from '../components/Header.js';
+import Loading from '../components/Loading.js';
+
+// Effect, State
+import { useEffect, useState } from 'react';
+
+// Css
+import '../routes/Home.scss'
 
 function Home(){
+    // loading이 true면 loading글 보여주고 false면 안보여주는 state
+    const [loading, setLoading] = useState(true);
+
     return(
         <div>
             <Header />
-            <p>Home PAGE</p>
+            {loading ? <Loading /> : 
+                <div>
+                </div>
+            }
+
         </div>
     )   
 }
