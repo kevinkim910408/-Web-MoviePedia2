4d8b86c60d2e351ee4aafe3936146466bd720c2d
kvv(Kevin Junho Kim) <59503331+kevinkim910408@users.noreply.github.com>
2022-05-10 04:29:17 -0400

    Home.js Done
    Home.js Done

--- static/myapp/src/routes/Home.js
+++ static/myapp/src/routes/Home.js
@@ -1,25 +1,23 @@
 // Components
 import Header from '../components/Header.js';
-import Loading from '../components/Loading.js';
-
-// Effect, State
-import { useEffect, useState } from 'react';
 
 // Css
 import '../routes/Home.scss'
 
 function Home(){
-    // loading이 true면 loading글 보여주고 false면 안보여주는 state
-    const [loading, setLoading] = useState(true);
+
 
     return(
         <div>
             <Header />
-            {loading ? <Loading /> : 
-                <div>
+                <div className='flexRow'>
+                    <div className="imgs"></div>
+                    <div className="imgs"></div>
+                    <div className="imgs"></div>
+                    <div className="imgs"></div>
+                    <div className="imgs"></div>
+                    <div className="imgs"></div>
                 </div>
-            }
-
         </div>
     )   
 }
